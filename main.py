1464ac7d92731eb77efea3e58c47c9c96fbed184
Arieh Norton <arieh.norton@gmail.com>
2021-12-19 22:22:42 +0200

    main

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
 
+def main():
+    # id1 = 3
+    # test = {1: {1: 2, 2: 3, 4: 3}, 2: {3: 2, 1: 2, 0: 2}}
+    # # test['innerdict']['test1'] = 21
+    # # test['innerdict1'] = {}
+    # # test['innerdict1']['test2'] = "hello"
+    # # test['innerdict1']['test3'] = "bye"
+    # # test[id1] = {}
+    # # test[id1]['test4'] = "testing"
+    # # print(len(test))
+    # for i in test:
+    #     for j in test[i].values():
+    #         print(j)
+    # for i in test:
+    #     print(test[i].values())
+    # print(test[1].get(1))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    list1 = {1: 2, 2: 2, 3: 3, 4: 4, 5: 5}
+    for i in list1.keys():
+        print(list1[i])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
