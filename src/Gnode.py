714922271c1640c269d4d10c85aacc551ea99f68
Arieh Norton <arieh.norton@gmail.com>
2021-12-23 16:58:29 +0200

    Gnode, changed init

--- src/Gnode.py
+++ src/Gnode.py
@@ -1,7 +1,17 @@
+import random
+
+
 class Gnode:
-    def __init__(self, key, location):
+    def __init__(self, key, location: tuple = None):
         self.key = key
-        self.location = location
+        if location is None:
+            x = random.uniform(35, 36)
+            y = random.uniform(32, 33)
+            location = (x, y, 0)
+            self.location = location
+        else:
+            self.location = location
+        self.tag = 0
 
     def __repr__(self):
         return 'Key: %s, location: %s' % (self.key, self.location)
